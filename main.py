f42d2995bd1471cc7e1aba774adc8f24799977c0
Harsh Karande <harshskarande@gmail.com>
2020-10-31 15:52:06 +0530

    feat: custom help embed

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ from discord.ext import commands
 from decouple import config
 
 client = commands.Bot(command_prefix='/')
+client.remove_command('help')
 
 
 @client.event
 async def on_ready():
     print('Bot is now online.')
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title='Ambassador | Help',
+        description="run commands by using '/' at prefix",
+        color=discord.Color.orange()
+    )
+    embed.set_thumbnail(
+        url='https://cdn.discordapp.com/avatars/766224438513369109/6477b9c304caa29f2124f993c0f0817b.png?size=256')
+    embed.add_field(name='help', value='get help about bot in server')
+    embed.add_field(name='ping', value='get bot latency')
+    embed.add_field(name='clear `int`', value='clear messages on server')
+    embed.add_field(name='kick `member`', value='kick member out of server', inline=True)
+    embed.set_footer(text='https://github.com/harshcut')
+    await ctx.send(embed=embed)
 
 
 @client.command()
